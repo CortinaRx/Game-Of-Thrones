13f341931dd579307173eeee26c8430c5ac36a43
CortinaRx <cortinarcz@gmail.com>
2022-02-06 19:51:31 +0100

    final Commit

---
+++ src/components/NotFound/NotFound.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { FormattedMessage } from "react-intl";
+
+const NotFound = () => {
+  return (
+
+    <p><FormattedMessage id="not" defaultMessage="Se desconoce..." /> </p>
+  )
+};
+
+export default NotFound;
